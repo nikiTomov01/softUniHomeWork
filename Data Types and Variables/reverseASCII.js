c3ff35e78d21c99d8e535683f841bf11c868bd84
Nik <nikolaitomov25@gmail.com>
2025-01-18 13:44:34 +0200

    finished second task

---
+++ Data Types and Variables/reverseASCII.js
@@ -0,0 +1,6 @@
+function reverseASCII(charOne, charTwo, charThree) {
+    console.log(`${charThree}${charTwo}${charOne}`);
+    console.log(charThree.charCodeAt(0), charTwo.charCodeAt(0), charOne.charCodeAt(0));
+}
+
+reverseASCII("1", "5", "p");
